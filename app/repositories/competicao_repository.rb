acc14a06e71316e0ffd9f2335a99b67c25027cf4
Harlley Bastos <harlleybastos@hotmail.com>
2024-02-27 14:10:16 -0300

    chore: remove unecessarie comment

--- app/repositories/competicao_repository.rb
+++ app/repositories/competicao_repository.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# CompeticaoRepository é responsável por gerenciar o acesso e persistência
-# dos dados relacionados às competições. Ele fornece uma abstração para
-# operações de banco de dados, permitindo que os serviços manipulem
-# competições sem depender diretamente do ActiveRecord.
 class CompeticaoRepository
   def create(nome, finalizada)
     Competicao.create(nome:, finalizada:)
